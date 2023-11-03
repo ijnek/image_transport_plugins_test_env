bc4b255b40484f417e66ad0223de1a25e4053d36
ijnek <kenjibrameld@gmail.com>
2023-11-03 01:30:07 +0000

    add cameras.urdf
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

---
+++ launch/visualize_launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'image_transport_plugins_test_env',
+            'urdf_package_path': PathJoinSubstitution(['urdf', 'cameras.urdf'])}.items()
+    ))
+
+    return ld
