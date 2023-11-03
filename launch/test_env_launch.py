52c756ceaced3331b039d00d5197bb5bc10997ce
ijnek <kenjibrameld@gmail.com>
2023-11-03 01:42:08 +0000

    update launch file
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- launch/test_env_launch.py
+++ launch/test_env_launch.py
@@ -1,17 +1,22 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    this_package = FindPackageShare('image_transport_plugins_test_env')
 
     ld.add_action(IncludeLaunchDescription(
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
         launch_arguments={
             'urdf_package': 'image_transport_plugins_test_env',
-            'urdf_package_path': PathJoinSubstitution(['urdf', 'cameras.urdf'])}.items()
+            'urdf_package_path': PathJoinSubstitution(['urdf', 'cameras.urdf']),
+            'jsp_gui': 'false',
+            'rviz_config': PathJoinSubstitution([this_package, 'rviz', 'urdf.rviz'])}.items()
     ))
 
     return ld
