e3cf1e597dd24be55d1a3c261325bce6ef4d17ab
ijnek <kenjibrameld@gmail.com>
2023-11-03 02:20:44 +0000

    add gazebo
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- launch/test_env_launch.py
+++ launch/test_env_launch.py
@@ -1,7 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
 
 from launch_ros.substitutions import FindPackageShare
 
@@ -17,6 +19,39 @@ def generate_launch_description():
             'urdf_package_path': PathJoinSubstitution(['urdf', 'cameras.urdf']),
             'jsp_gui': 'false',
             'rviz_config': PathJoinSubstitution([this_package, 'rviz', 'urdf.rviz'])}.items()
+    ))
+
+    # Gazebo
+    ld.add_action(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py'])),
+        launch_arguments={
+            'gz_args': [
+                '-r ',  # Run simulation on start.
+                PathJoinSubstitution([this_package, 'world', 'world.sdf']),
+            ]
+        }.items()
+    ))
+
+    # Create robot in Gazebo
+    ld.add_action(Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-z', '0.1',
+        ],
+    ))
+
+    # ROS <-> GZ Bridge
+    ld.add_action(Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # Clock (GZ -> ROS2)
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+        ],
     ))
 
     return ld
